1530dde0255db266f5bbd6f060390dfba8fc6cad
Zachary Winoker <zacharywinoker@Zacharys-MacBook-Pro.local>
2016-04-25 21:00:16 -0700

    make readme, clean up symbolslist.csv, clean up comments in Stock_Market.cpp

---
+++ README.md
@@ -0,0 +1,60 @@
+# Name
+
+This is a C++ framework for implementing and testing stock trading algorithms. It supports up to minute-level resolution price data. Data is assumed to be stored in a MySQL datatbase.  
+
+
+# Install
+
+The following libraries are required:
+1. MySQL connector
+2. Boost
+3. curl
+
+When these are installed, just navigate to the project directory and type ```make```. The executable is called ```run_simulation```.
+
+
+# Data
+Minute level data was scraped from Google Finance using the R script ```getPrices.r``` and stored in a MySQL database in the following format: Tables are named according to symbol (EX: "AAPL_", including the underscore). Each row in a table has the columns { Date-Time, Close, High, Low, Open, Volume } for each minute. 
+
+The symbols we collect data on and use are stored in symbolslist.csv.
+
+
+# Setting test parameters
+After compiling the executable, set parameters in ```simulation.in```. These are:
+```
+file where we save portfolio holdings
+
+start date-time
+
+end date-time
+
+live?
+
+tz_offset
+
+max_ticks
+
+frequency
+
+tradefrequency
+
+MYSQL_SERVER
+
+MYSQL_USERNAME
+
+MYSQL_PASSWORD
+
+MYSQL_DBNAME
+```
+
+```live?``` is a boolean that is ```false``` if we're running a backtest, and ```true``` otherwise. ```tz_offset`` is your timezone offset (in hours) from New York City. ````max_ticks``` is the number of previous time steps our algorithm will consider at any given time. ```frequency``` is the number of time steps between each stored data point. ```tradefrequency``` is the number of time steps we wait between successive executions of the trading algorithm.
+
+# Usage
+Modify ```algo.cpp```` to implement and execute your trading algorithm. The simulation interacts with the trading algorithm by calling ````algo.eval()```, so modify this method when implementing a trading system.
+
+Once the input parameters are set and the trading system is implemented, run 
+```make
+
+./run_simulation < simulation.in```
+
+
--- Stock_Market.cpp
+++ Stock_Market.cpp
@@ -96,109 +96,12 @@ Stock_Market::Stock_Market(int frequency, int numticks, string startdate,
 	
 }
 
-/* Version with fewer sql queries */
-// Stock_Market::Stock_Market(int frequency, int numticks, int future_ticks, string startdate, 
-// 						   const char* srvr, const char* uname, const char* pw, const char* dbase) {
-
-// 	/* Wil help point to current tick datum to consider */
-// 	current_elt = numticks - 1;
-// 	earliest_elt = 0;
-
-// 	/* Convert SQL database params for the MYSQL_connect library to read */
-// 	const char *server   = srvr;
-// 	const char *username = uname;
-// 	const char *password = pw;
-// 	const char *database = dbase;
-
-//     /* Populate symbols list */
-//     string sym;
-// 	ifstream symbols;
-// 	symbols.open("symbolslist.csv");
-
-// 	while(getline(symbols,sym,',')) {
-// 		symbol_list.push_back(sym);
-// 	}
-// 	symbols.close();
-//     vector<string>::iterator it;
-
-//     MYSQL mysql;
-
-// 	mysql_init(&mysql);
-// 	mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"your_prog_name");
-// 	if (!mysql_real_connect(&mysql,server,username,password,database,0,NULL,0))
-// 	{
-// 	    fprintf(stderr, "Failed to connect to database: Error: %s\n",
-// 	          mysql_error(&mysql));
-// 	}
-
-// 	/* Open a connection to the MySQL database*/
-// 	MYSQL_ROW row;
-
-// 	unsigned int num_fields;
-// 	unsigned int i;
-
-// 	/* tick data */
-// 	string thistime;
-// 	vector<double> prices;
-// 	tick_data ttd;
-// 	vector<tick_data> pdata;
-
-//     /* Populate local market data table from the MySQL database. See Scraper::backtest_scrape() for more info */
-//     for (it = symbol_list.begin(); it != symbol_list.end(); ++it) {
-// 		//string s = "select * from (select * from " + ((*it) + "_") + " where Date < \'" + startdate + "\' and time(Date) < '16:00:00' and time(date) >= '09:30:00' order by Date desc limit " + to_string(frequency*numticks) + ") as bigdates where timestampdiff(minute, \'" + startdate + "\', Date) mod " + to_string(frequency) + " = 0 order by Date";
-// 		string s = "select * from (select * from (select * from " + ((*it) + "_") + " where Date <= \'" + startdate + "\' and time(Date) < '16:00:00' and time(date) >= '09:30:00' order by date desc limit " + to_string(frequency*numticks) + ") as bigdates where timestampdiff(minute, \'" + startdate + "\', Date) mod " + to_string(frequency) + " = 0 order by Date) as t1 union select * from (select * from (select * from " + ((*it) + "_") + " where Date > \'" + startdate + "\' and time(Date) < '16:00:00' and time(date) >= '09:30:00' order by date limit " + to_string(frequency*future_ticks) + ") as bigdates where timestampdiff(minute, \'" + startdate + "\', date) mod " + to_string(frequency) + " = 0 order by date) as t2";
-
-
-// 		mysql_query(&mysql, s.c_str());
-//         if(*mysql_error(&mysql))
-// 		{
-// 		  fprintf(stderr, "Error: %s\n", mysql_error(&mysql));
-// 		}
-
-//     	MYSQL_RES *result;
-//     	result = mysql_use_result(&mysql);
-
-// 		num_fields = mysql_num_fields(result);
-		
-// 		while ((row = mysql_fetch_row(result)))
-// 		{
-// 		   unsigned long *lengths;
-// 		   lengths = mysql_fetch_lengths(result);
-
-// 		   thistime = row[0];
-		   
-// 		   for(i = 1; i < num_fields; i++){
-// 		   		prices.insert(prices.end(), stod(string(row[i])));
-// 		   }
-// 		   ttd.first = thistime;
-// 		   ttd.second = prices;
-// 		   pdata.insert(pdata.begin(), ttd);
-// 		   prices.clear();
-// 		}
-// 		marketdata[*it] = pdata;
-// 		mysql_free_result(result);
-// 		pdata.clear();
-		
-//     }
-//     mysql_close(&mysql);
-	
-// }
-
-
-
 
 /* Returns a single tick datum (closing price, opening price, etc) */
 double Stock_Market::get_info(string symbol, int info) {
 	return (marketdata[symbol].front().second)[info];
 
 }
-
-//Version for faster backtests
-/* Returns a single tick datum (closing price, opening price, etc) */
-// double Stock_Market::get_info(string symbol, int info) {
-// 	return (marketdata[symbol][current_elt].second)[info];
-
-// }
 
 /* Returns an entire vector of tick data for a single time */
 vector<tick_data> Stock_Market::get_info_matrix(string symbol, int numsteps, int freq) {
@@ -243,11 +146,6 @@ bool Stock_Market::check_date(string symbol){
 	return(marketdata[symbol].front().first == workingtime);
 }
 
-//Version for faster backtesting
-/* Checks that current date and last recorded date on a tick datum are the same */
-// bool Stock_Market::check_date(string symbol){
-// 	return(marketdata[symbol][current_elt].first == workingtime);
-// }
 
 /* Prints stock market data to the console */
 void Stock_Market::show() {
